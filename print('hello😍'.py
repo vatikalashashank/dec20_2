b182b08b0220df53f10eefd4dc90041b08a79be5
vatikalashashank <vatikalashashank@gmail.com>
2024-12-20 19:58:45 +0530

    Create print('hello😍'.py

---
+++ print('hello😍'.py
@@ -0,0 +1,27 @@
+# print('hello😍'.upper())
+
+#  print('A','ord--->',ord('A'))
+#  print('a','ord--->',ord('a'))
+
+
+# val='ABC'
+# for x in val:
+#     ord_val=ord(x)
+#     conv_chr=ord_val+32
+#     print(chr(conv_chr))
+
+# val='ABcTuuO'
+# for x in val:
+#     ord_val=ord(x)
+# if ord_val in range(65,91):
+#     print(chr(ord_val+32),end='')
+# elif ord_val in range (97,123):
+#     print(chr(ord_val-32),end='')
+# else:
+#     print(x, end='')
+          
+print('A','ord--->',ord('A'))
+print('a','ord--->',ord('a'))
+
+print(65,'chr--->',chr(65))
+print(97,'chr--->',chr(97))
